c46d924b03b5bb8fa0d95d6479a6c85ea2f3e5c4
Goksel Eryigit <goksel@carbonhealth.com>
2024-05-20 16:19:36 +0300

    keys

--- config/plugins.js
+++ config/plugins.js
@@ -14,11 +14,10 @@ module.exports = () => ({
   "vercel-deploy": {
     enabled: true,
     config: {
-      deployHook:
-        "https://api.vercel.com/v1/integrations/deploy/prj_eudd88cKD2TAP9NqrMJ098WhCXbH/bkr3vasmbm",
-      apiToken: "WgIDpbDL0tSzii3KEHIoYCGh",
-      appFilter: "geryit-admin2",
-      teamFilter: "team_rMefstvfq2arTW8P8q4nn0IB",
+      deployHook: process.env.VERCEL_DEPLOY_PLUGIN_HOOK,
+      apiToken: process.env.VERCEL_DEPLOY_PLUGIN_API_TOKEN,
+      appFilter: process.env.VERCEL_DEPLOY_PLUGIN_APP_FILTER,
+      teamFilter: process.env.VERCEL_DEPLOY_PLUGIN_TEAM_FILTER,
       roles: ["strapi-super-admin", "strapi-editor", "strapi-author"],
     },
   },
